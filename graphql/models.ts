e555b079209d90dcde3214faab98318d1b1c66a5
Emmanuel <flotuz10@gmail.com>
2022-10-15 12:51:47 -0500

    REFACTOR: Models and export of dtos

--- graphql/models.ts
+++ graphql/models.ts
@@ -3,3 +3,30 @@ export * from "./common/common.types";
 //================ USER ====================
 export * from "./user/user.entity";
 export * from "./user/user.dto";
+
+//================ BECA ====================
+export * from "./beca/beca.dto";
+
+//================ BECARIO ====================
+export * from "./becario/becario.dto";
+
+//================ CAFETERIA ====================
+export * from "./cafeteria/cafeteria.dto";
+
+//================ COBRO ====================
+export * from "./cobro/cobro.dto";
+
+//================ IMAGEN ====================
+export * from "./imagen/imagen.dto";
+
+//================ PERSONA ====================
+export * from "./persona/persona.dto";
+
+//================ PROBLEMA ====================
+export * from "./problema/problema.dto";
+
+//================ ROLE ====================
+export * from "./role/role.dto";
+
+//================ SETTINGS ====================
+export * from "./settings/settings.dto";
